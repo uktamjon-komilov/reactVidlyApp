1681a7ab883f037e02a9e26914c25a7487cc8848
Uktamjon Komilov <uktamforjob@gmail.com>
2021-01-31 17:54:58 +0500

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { Component } from 'react';
+import { Component } from "react";
 import React from "react";
-import './App.css';
-import Movies from './components/movies';
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 
 class App extends Component {
-
-  render(){
+  render() {
     return (
-      <main className="container p-5">
-        <Movies/>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container p-5">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/notFound" component={NotFound} />
+            <Redirect path="/" exact to="/movies" />
+            <Redirect to="notFound" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Vidly
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <NavLink className="nav-item nav-link" to="/movies">
+              Movies
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/customers">
+              Customers
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
